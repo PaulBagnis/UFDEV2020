5896eee66d70390efaa1401996796dfef001b7ae
Ynovbanj0 <paul.bagnis@ynov.com>
2020-06-16 15:31:18 +0200

    les randoms ennemys

--- classes.py
+++ classes.py
@@ -21,8 +21,6 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 
 # Classe pour les vaisseaux
 class Ship:
-    COOLDOWN = 30
-
     # Constructeur
     def __init__(self, x, y, health=100):
         self.x = x
@@ -31,20 +29,12 @@ class Ship:
         self.ship_look = None
         self.laser_look = None
         self.lasers = []
-        self.cool_down_counter = 0
 
     # Fonction pour Render le vaisseau sur la fenêtre de jeu (fonction blit() de pygame)
     def draw(self, window):
         window.blit(self.ship_look, (self.x, self.y))
         for laser in self.lasers:
             laser.draw(window)
-
-    # Définition du temps entre deux tirs
-    def cooldown(self):
-        if self.cool_down_counter >= self.COOLDOWN:
-            self.cool_down_counter = 0
-        elif self.cool_down_counter > 0:
-            self.cool_down_counter += 1
 
     # Méthode qui permet le tir d'un laser
     def shoot(self):
@@ -73,15 +63,17 @@ class Ship:
 
 # Classe pour le joueur
 class Player(Ship):
+    COOLDOWN = 30
     # Constructeur
-    def __init__(self, x, y, health=100):
+    def __init__(self, x, y, look, health=100):
         super().__init__(x, y, health)
-        self.ship_look = YELLOW_SPACE_SHIP
+        self.ship_look = look
         self.laser_look = YELLOW_LASER
         # Le mask sert a parcourir les images pour détecter les pixels effectifs des png sur 
         # la surface pour une meilleur gestion des impacts
         self.mask = pygame.mask.from_surface(self.ship_look)
         self.max_health = health
+        self.cool_down_counter = 0
 
     # Fonction pour Render le vaisseau sur la fenêtre de jeu (Overxrite celle du parent)
     def draw(self, window):
@@ -102,6 +94,13 @@ class Player(Ship):
                         if laser in self.lasers:
                             self.lasers.remove(laser)
 
+    # Définition du temps entre deux tirs
+    def cooldown(self):
+        if self.cool_down_counter >= self.COOLDOWN:
+            self.cool_down_counter = 0
+        elif self.cool_down_counter > 0:
+            self.cool_down_counter += 1
+
     # Barre de vie du joueur
     def healthbar(self, window):
         pygame.draw.rect(window, (255,0,0), (self.x, self.y + self.ship_look.get_height() + 10, self.ship_look.get_width(), 10))
@@ -110,20 +109,29 @@ class Player(Ship):
 
 # Classe pour les vaisseaux hostiles
 class Enemy(Ship):
-    COOLDOWN = 30
-
     # Différentes apparences des vaisseaux
     COLOR_MAP = {
-                "red": (RED_SPACE_SHIP, RED_LASER),
-                "green": (GREEN_SPACE_SHIP, GREEN_LASER),
-                "blue": (BLUE_SPACE_SHIP, BLUE_LASER)
+                "1": (RED_SPACE_SHIP, RED_LASER),
+                "2": (GREEN_SPACE_SHIP, GREEN_LASER),
+                "3": (BLUE_SPACE_SHIP, BLUE_LASER)
                 }
     # Constructeur
-    def __init__(self, x, y, color, health=100):
-        super().__init__(x, y, health)
+    def __init__(self, x, y, color, health, vel, cd):
+        super().__init__(x, y)
         self.ship_look, self.laser_look = self.COLOR_MAP[color]
         self.mask = pygame.mask.from_surface(self.ship_look)
+        self.health = health
         self.max_health = 100
+        self.vel = vel
+        self.cool_down_counter = 0
+        self.cd = cd
+
+    # Définition du temps entre deux tirs
+    def cooldown(self):
+        if self.cool_down_counter >= self.cd:
+            self.cool_down_counter = 0
+        elif self.cool_down_counter > 0:
+            self.cool_down_counter += 1
 
     # Fait de tirer
     def shoot(self):
@@ -138,8 +146,8 @@ class Enemy(Ship):
         pygame.draw.rect(window, (0,255,0), (self.x, self.y + self.ship_look.get_height() + 10, self.ship_look.get_width() * (self.health/self.max_health), 10))
 
     # Méthode pour bouger
-    def move(self, vel):
-        self.y += vel
+    def move(self):
+        self.y += self.vel
 
 class Laser:
     # Constructeurs
